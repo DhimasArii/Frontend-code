3c748e35bf76f830155d3bb8d547d2ab0462733a
masar4073 <mbahgenius123@gmail.com>
2024-03-25 12:32:49 +0700

    fix tampilan menu-class

--- src/pages/landing-page/landing.jsx
+++ src/pages/landing-page/landing.jsx
@@ -119,7 +119,7 @@ const Landing = ({ isLoggedIn, setIsLoggedIn }) => {
                         <CardComponent
                           title={item.title}
                           body={item.title}
-                          image={item.url}
+                          image={"/images/image-body-confirm.png"}
                         />
                       </Grid>
                     );
