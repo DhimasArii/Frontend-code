41e0cb85e431eb63e3edacf17efbf402b14cf155
DhimasArii <dhimasari03@gmail.com>
2024-03-26 21:55:30 +0700

    Check Out & Payment

---
+++ src/components/CardCheckbox.jsx
@@ -0,0 +1,39 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import ImageCourse from "../assets/image-course.png";
+import { CardActionArea } from "@mui/material";
+import "../components/style.css";
+
+export default function ActionAreaCard({ title, body, image }) {
+  return (
+    <Card sx={{ width: 1017, gap: 24 }}>
+      <CardActionArea sx={{ display: "flex", flexDirection: "row" }}>
+        <CardMedia
+          component="img"
+          width="200"
+          height="133.33"
+          src={image}
+          alt="green iguana"
+        />
+        <div className="flex flex-col">
+          <CardContent sx={{}}>
+            <div className=" text-left text-16 font-400 text-gray font-montserrat">
+              {title}
+            </div>
+            <div className=" text-left text-18 font-600 font-montserrat">
+              {body}
+            </div>
+          </CardContent>
+          <CardContent sx={{}}>
+            <div className=" text-yellow font-600 font-montserrat text-20 ">
+              IDR 400.000
+            </div>
+          </CardContent>
+        </div>
+      </CardActionArea>
+    </Card>
+  );
+}
